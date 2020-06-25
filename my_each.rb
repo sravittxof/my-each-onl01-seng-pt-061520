5a2f9a72aebc7b70acdb44e329fb612c7b7d1bfe
Travis Fox <travis.fox90@gmail.com>
2020-06-25 01:34:32 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  
+  return array
+  
+end
+
+collection = [1, 2, 3, 4]
+
+my_each(collection) do |i|
+  print i
+  '\n'
 end
